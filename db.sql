9f1981356b0d6243598f6e175b1bd16f53d89588
NastuzziSamy <samy@nastuzzi.fr>
2020-02-15 19:52:40 +0100

    Wtf?

--- db.sql
+++ db.sql
@@ -156,7 +156,7 @@ CREATE TABLE `students` (
   `nbrUV` int(11) NOT NULL,
   `uvs` char(64) COLLATE utf8_bin NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1',
-  `mode` char(16) COLLATE utf8_bin DEFAULT NULL
+  `mode` char(16) COLLATE utf8_bin DEFAULT NULL,
   `prevoir` text COLLATE utf8_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
